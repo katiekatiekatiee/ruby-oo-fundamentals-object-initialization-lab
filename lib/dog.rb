64e87485f1600955b27f122a71968f87b607fe54
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-03-09 22:25:23 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,43 @@
+# class Dog
+#     def initialize(name)
+#         @name = name
+#     end
+#     def name= (name)
+#         @name = name
+#     end
+#     def name
+#         @name
+#     end
+
+#     def initialize(breed)
+#         @breed = breed
+#     end
+#     def breed= (breed)
+#         @breed = breed
+#     end
+#     def breed
+#         @breed
+#     end
+
+# end
+
+class Dog 
+    def initialize(name, breed = "Mutt")
+        @name= name
+        @breed= breed
+    end
+    def name= (name)
+        @name = name
+    end
+    def breed= (breed)
+        @breed = breed
+    end
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
+   
+end
+
